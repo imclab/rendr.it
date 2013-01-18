c736d971e8d8798d480b78ef39358b29cc4a8816
Reed Murphy <reed@reedmurphy.net>
2013-01-18 13:04:15 +1100

    pycollectd network protocol rewrite

--- rendr/pycollectd/__init__.py
+++ rendr/pycollectd/__init__.py
@@ -21,14 +21,91 @@ import time
 
 from tornado import ioloop
 
-import constants
-import utils
+__all__ = [
+    "CollectdClient", "PycollectdException",
+    "sanitize", "count_to_value_part",
+    "part_type_to_data_type", "value_to_part"
+    "PART_TYPE", "VALUE_TYPE", "MAX_PACKET_LENGTH", "DEFAULT_PLUGIN_NAME",
+    "DEFAULT_PLUGIN_INSTANCE", "DEFAULT_PLUGIN_TYPE", "DEFAULT_SEND_INTERVAL",
+    "DEFAULT_PORT", "DEFAULT_CUMM_FUNCTION",
+]
 
-__all__ = ["CollectdClient"]
+# Constants
 
-__version_info__ = (1, 0, 3, "devel", 0)
-__version__ = "{0}.{1}.{2}".format(*__version_info__) # pylint: disable=W0142
+PART_TYPE = {
+    'HOST': 0x0000,
+    'TIME': 0x0001,
+    'PLUGIN': 0x0002,
+    'PLUGIN_INSTANCE': 0x0003,
+    'TYPE': 0x0004,
+    'TYPE_INSTANCE': 0x0005,
+    'VALUES': 0x0006,
+    'INTERVAL': 0x0007,
+    'TIME_HR': 0x0008,
+    'INTERVAL_HR': 0x0009,
+    'MESSAGE': 0x0100,
+    'SEVERITY': 0x0101,
+    'SIGNATURE': 0x0200,
+    'ENCRYPTION': 0x0210
+}
 
+VALUE_TYPE = {
+    'COUNTER': (0x00, '>Q'),
+    'GAUGE': (0x01, '<d'),
+    'DERIVE': (0x02, '>q'),
+    'ABSOLUTE': (0x03, '>Q')
+}
+
+MAX_PACKET_LENGTH = 1024
+
+DEFAULT_PLUGIN_NAME = "any"
+DEFAULT_PLUGIN_INSTANCE = ""
+DEFAULT_PLUGIN_TYPE = "gauge"
+DEFAULT_SEND_INTERVAL = 60  # seconds
+DEFAULT_PORT = 25826
+DEFAULT_CUMM_FUNCTION = sum
+
+# Utility Functions
+
+def sanitize(string):
+    """Sanitizes a metric name."""
+    return re.sub(r"[^a-zA-Z0-9]+", "_", string).strip("_")
+
+def count_to_value_part(name, count, value_type='GAUGE'):
+    data_type, pack_format = VALUE_TYPE[value_type]
+    count_value = struct.pack(pack_format, count)
+    return ''.join([
+        value_to_part(PART_TYPE['TYPE_INSTANCE'], name),
+        struct.pack('>HHHB', PART_TYPE['VALUES'], len(count_value) + 7, 1, data_type), count_value
+    ])
+
+def part_type_to_data_type(part_type):
+    if part_type in (
+            PART_TYPE['TIME'], PART_TYPE['TIME_HR'],
+            PART_TYPE['INTERVAL'], PART_TYPE['INTERVAL_HR']):
+        return 'numeric'
+    elif part_type in (
+            PART_TYPE['HOST'], PART_TYPE['PLUGIN'],
+            PART_TYPE['PLUGIN_INSTANCE'], PART_TYPE['TYPE'],
+            PART_TYPE['TYPE_INSTANCE']):
+        return 'string'
+    else:
+        raise PycollectdException("Unknown or unimplemented part_type {}".format(part_type))
+
+def value_to_part(part_type, value):
+    # part_type(2), length(2), payload
+    data_type = part_type_to_data_type(part_type)
+    if data_type == 'numeric':
+        return struct.pack('>HHq', part_type, 12, value)
+    elif data_type == 'string':
+        return struct.pack('>HH', part_type, len(value) + 5) + value + '\0'
+    else:
+        raise PycollectdException("Invalid data_type {}".format(data_type))
+
+# Classes
+
+class PycollectdException(Exception):
+    pass
 
 class CollectdClient(object): # pylint: disable=R0902
     """
@@ -68,17 +145,17 @@ class CollectdClient(object): # pylint: disable=R0902
                        `ioloop.IOLoop.instance()`
         """
 
-        collectd_port = kwargs.pop("collectd_port", constants.DEFAULT_PORT)
+        collectd_port = kwargs.pop("collectd_port", DEFAULT_PORT)
         self.collectd_addr = (collectd_hostname, collectd_port)
         self.hostname = kwargs.pop("hostname", socket.getfqdn())
         self.plugin_name = kwargs.pop(
-            "plugin_name", constants.DEFAULT_PLUGIN_NAME)
+            "plugin_name", DEFAULT_PLUGIN_NAME)
         self.plugin_instance = kwargs.pop(
-            "plugin_instance", constants.DEFAULT_PLUGIN_INSTANCE)
+            "plugin_instance", DEFAULT_PLUGIN_INSTANCE)
         self.plugin_type = kwargs.pop(
-            "plugin_type", constants.DEFAULT_PLUGIN_TYPE)
+            "plugin_type", DEFAULT_PLUGIN_TYPE)
         self.send_interval = kwargs.pop(
-            "send_interval", constants.DEFAULT_SEND_INTERVAL)
+            "send_interval", DEFAULT_SEND_INTERVAL)
         self.io_loop = kwargs.pop("io_loop", ioloop.IOLoop.instance())
 
         self._queue = collections.deque()
@@ -149,14 +226,14 @@ class CollectdClient(object): # pylint: disable=R0902
         summed_values = {}
         functions = {}
         for metric, value, cumm_func in self._queue:
-            metric = utils.sanitize(metric)
+            metric = sanitize(metric)
             values_by_metric[metric].append(value)
             if cumm_func is not None:
                 functions[metric] = cumm_func
         self._queue.clear()
 
         for metric, values in values_by_metric.iteritems():
-            cumm_func = functions.get(metric, constants.DEFAULT_CUMM_FUNCTION)
+            cumm_func = functions.get(metric, DEFAULT_CUMM_FUNCTION)
             summed_values[metric] = cumm_func(values)
         return summed_values
 
@@ -168,46 +245,33 @@ class CollectdClient(object): # pylint: disable=R0902
         """
         values_sent = 0
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        for packet in self._create_packets(values):
+        for packet in self.counts_to_packets(values):
             bytes_tx = sock.sendto(packet, self.collectd_addr)
             if len(packet) == bytes_tx:
                 values_sent += 1
         return values_sent
 
-    def _generate_message_start(self, when=None):
-        """
-        Returns the collectd packet header
-        """
-        return "".join([
-            utils.pack(constants.TYPE_HOST, self.hostname),
-            utils.pack(constants.TYPE_TIME, when or time.time()),
-            utils.pack(constants.TYPE_PLUGIN, self.plugin_name),
-            utils.pack(constants.TYPE_PLUGIN_INSTANCE, self.plugin_instance),
-            utils.pack(constants.TYPE_TYPE, self.plugin_type),
-            utils.pack(constants.TYPE_INTERVAL, self.send_interval)
-        ])
+    def counts_to_packets(self, counts, timestamp=None):
+        packet = self.head_part(timestamp)
+        for name, count in counts.iteritems():
+            count_part = count_to_value_part(name, count)
+            if len(packet) + len(count_part) >= MAX_PACKET_LENGTH:
+                yield packet
+                packet = self.head_part(timestamp)
+            packet += count_part
+        yield packet
 
-    def _create_packets(self, counts, when=None):
-        """
-        Converts summarized counts into collectd UDP packets
-        """
-        packets = []
-        start = self._generate_message_start(when)
-        parts = [utils.pack(name, count) for name, count in counts.items()]
-        parts = [
-            p for p in parts
-            if len(start) + len(p) <= constants.MAX_PACKET_SIZE
-        ]
-        if parts:
-            curr, curr_len = [start], len(start)
-            for part in parts:
-                if curr_len + len(part) > constants.MAX_PACKET_SIZE:
-                    packets.append("".join(curr))
-                    curr, curr_len = [start], len(start)
-                curr.append(part)
-                curr_len += len(part)
-            packets.append("".join(curr))
-        return packets
+    def head_part(self, timestamp=None):
+        timestamp = timestamp or time.time()
+
+        return ''.join([
+            value_to_part(PART_TYPE['HOST'], self.hostname),
+            value_to_part(PART_TYPE['TIME'], timestamp),
+            value_to_part(PART_TYPE['PLUGIN'], self.plugin_name),
+            value_to_part(PART_TYPE['PLUGIN_INSTANCE'], self.plugin_instance),
+            value_to_part(PART_TYPE['TYPE'], self.plugin_type),
+            value_to_part(PART_TYPE['INTERVAL'], self.send_interval)
+        ])
 
     # Predefined summarizing functions
     @staticmethod
@@ -237,63 +301,63 @@ if __name__ == "__main__":
                 0x00, 0x00, 0x00, 0x3c
             ])
 
-            header = self.client._generate_message_start(1)
+            header = self.client.head_part(1)
             self.assertEqual(header, expected)
 
         def test_sanitize(self):
-            self.assertEqual(utils.sanitize(string.ascii_letters), string.ascii_letters)
-            self.assertEqual(utils.sanitize(string.digits), string.digits)
-            self.assertEqual(utils.sanitize("`~!@#$%^&*()-_=+[{]};:'\",<.>/? "), "")
-            self.assertEqual(utils.sanitize("~~this~~is~~a~~test~~string~~"), "this_is_a_test_string")
+            self.assertEqual(sanitize(string.ascii_letters), string.ascii_letters)
+            self.assertEqual(sanitize(string.digits), string.digits)
+            self.assertEqual(sanitize("`~!@#$%^&*()-_=+[{]};:'\",<.>/? "), "")
+            self.assertEqual(sanitize("~~this~~is~~a~~test~~string~~"), "this_is_a_test_string")
 
 
         def test_pack_numeric(self):
-            self.assertEqual(utils.pack_numeric(constants.TYPE_TIME, -1),
+            self.assertEqual(value_to_part(PART_TYPE['TIME'], -1),
                 "".join(chr(x) for x in [
                     0x00, 0x01, 0x00, 0x0c, 0xff, 0xff, 0xff, 0xff,
                     0xff, 0xff, 0xff, 0xff
                 ]))
-            self.assertEqual(utils.pack_numeric(constants.TYPE_TIME, 1),
+            self.assertEqual(value_to_part(PART_TYPE['TIME'], 1),
                 "".join(chr(x) for x in [
                     0x00, 0x01, 0x00, 0x0c, 0x00, 0x00, 0x00, 0x00,
                     0x00, 0x00, 0x00, 0x01
                 ]))
 
         def test_pack_string(self):
-            self.assertEqual(utils.pack_string(constants.TYPE_HOST, "hostname"),
+            self.assertEqual(value_to_part(PART_TYPE['HOST'], "hostname"),
                     "".join(chr(x) for x in [
                         0x00, 0x00, 0x00, 0x0d, 0x68, 0x6f, 0x73, 0x74,
                         0x6e, 0x61, 0x6d, 0x65, 0x00
                 ]))
-            self.assertEqual(utils.pack_string(constants.TYPE_PLUGIN, "plugin"),
+            self.assertEqual(value_to_part(PART_TYPE['PLUGIN'], "plugin"),
                     "".join(chr(x) for x in [
                         0x00, 0x02, 0x00, 0x0b, 0x70, 0x6c, 0x75, 0x67,
                         0x69, 0x6e, 0x00
                 ]))
 
         def test_pack_value(self):
-            self.assertEqual(utils.pack_value("value", 1),
+            self.assertEqual(count_to_value_part("value", 1),
                     "".join(chr(x) for x in [
                         0x00, 0x05, 0x00, 0x0a, 0x76, 0x61, 0x6c, 0x75,
                         0x65, 0x00, 0x00, 0x06, 0x00, 0x0f, 0x00, 0x01,
                         0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0,
                         0x3f
                 ]))
-            self.assertEqual(utils.pack_value("value", -1),
+            self.assertEqual(count_to_value_part("value", -1),
                     "".join(chr(x) for x in [
                         0x00, 0x05, 0x00, 0x0a, 0x76, 0x61, 0x6c, 0x75,
                         0x65, 0x00, 0x00, 0x06, 0x00, 0x0f, 0x00, 0x01,
                         0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0,
                         0xbf
                 ]))
-            self.assertEqual(utils.pack_value("value", 0),
+            self.assertEqual(count_to_value_part("value", 0),
                     "".join(chr(x) for x in [
                         0x00, 0x05, 0x00, 0x0a, 0x76, 0x61, 0x6c, 0x75,
                         0x65, 0x00, 0x00, 0x06, 0x00, 0x0f, 0x00, 0x01,
                         0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                         0x00
                 ]))
-            self.assertEqual(utils.pack_value("value", 12345),
+            self.assertEqual(count_to_value_part("value", 12345),
                     "".join(chr(x) for x in [
                         0x00, 0x05, 0x00, 0x0a, 0x76, 0x61, 0x6c, 0x75,
                         0x65, 0x00, 0x00, 0x06, 0x00, 0x0f, 0x00, 0x01,
